08f01b424b5d1c9d25f80c38847cd933a42e3305
Aschelde <asger.foulum@gmail.com>
2021-04-29 15:00:22 +0200

    Moved test to activitySteps

--- projekt_20/src/test/java/dtu/company/app/ActivitySteps.java
+++ projekt_20/src/test/java/dtu/company/app/ActivitySteps.java
@@ -1,5 +1,9 @@
 package dtu.company.app;
 
+import static org.junit.Assert.assertTrue;
+
+import io.cucumber.java.en.Given;
+
 public class ActivitySteps {
     private CompanyApp companyApp;
     private ErrorMessageHolder errorMessage;
@@ -9,5 +13,9 @@ public class ActivitySteps {
         this.errorMessage = errorMessage;
     }
 
-
+    @Given("there exists an activity {string} with id <{int}> in project {string}")
+	public void there_exists_an_activity_with_id_in_project(String string, Integer int1, String string2) {
+		assertTrue(companyApp.getProject(string2).containsActivityWithID(int1));
+		assertTrue(companyApp.getProject(string2).getActivityWithID(int1).getActivityName().equals(string));
+	}
 }
--- projekt_20/src/test/java/dtu/company/app/EmployeeSteps.java
+++ projekt_20/src/test/java/dtu/company/app/EmployeeSteps.java
@@ -22,12 +22,6 @@ public class EmployeeSteps {
 		this.errorMessage = errorMessage;
 	}
 
-	@Given("there exists an activity {string} with id <{int}> in project {string}")
-	public void there_exists_an_activity_with_id_in_project(String string, Integer int1, String string2) {
-		assertTrue(companyApp.getProject(string2).containsActivityWithID(int1));
-		assertTrue(companyApp.getProject(string2).getActivityWithID(int1).getActivityName().equals(string));
-	}
-
 	@Given("there exists an employee with id <{int}> which is project leader for project {string}")
 	public void there_exists_a_project_leader_with_id(Integer int1, String string) {
 		assertTrue(companyApp.containsEmployeeWithId(int1));
