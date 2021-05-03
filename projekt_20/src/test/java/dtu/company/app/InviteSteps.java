e5a3087321ea0408c2f4256b6547a077f9d27bb9
s204442 <s204442@student.dtu.dk>
2021-05-03 14:26:09 +0200

    Fixed invitesteps failed feature
    missing re-declaration of employees in When step inside invitesteps

--- projekt_20/src/test/java/dtu/company/app/InviteSteps.java
+++ projekt_20/src/test/java/dtu/company/app/InviteSteps.java
@@ -33,23 +33,18 @@ public class InviteSteps {
 	    this.companyApp.addNewEmployee(this.inviter);
 	    this.project.addEmployee(this.inviter);
 	    this.activity.assignEmployee(this.inviter.getId(), 0);
-//	    System.out.println(this.inviter.getId());
 	}
 
 	@Given("employee <{int}> exists within companyApp")
 	public void employee_exists_within_company_app(Integer int1) {
 	    this.invitee = new Employee(int1);
 	    this.companyApp.addNewEmployee(invitee);
-//	    System.out.println(this.invitee.getId());
 
 	}
 
 	@When("employee <{int}> invites employee <{int}> to assist with activity <{int}>")
 	public void employee_invites_employee_to_assist_with_activity(Integer int1, Integer int2, Integer int3) throws Exception {
-//	    this.activity.inviteEmployee(this.inviter, this.invitee);
-//	    System.out.println(this.inviter.getId());
-//	    System.out.println(this.invitee.getId());
-//	    assertTrue(this.activity.containsEmployeeWithID(this.invitee.getId()));
+//	    Der burde måske være noget her men det virker vel uden 
 	}
 
 	@Then("assign employee <{int}> to activity <{int}>")
@@ -64,17 +59,23 @@ public class InviteSteps {
 	}
 	
 	@When("employee <{int}> is invited to activity <{int}> where it already exists")
-	public void employee_is_invited_to_activity_where_it_already_exists(Integer int1, Integer int2) {
+	public void employee_is_invited_to_activity_where_it_already_exists(Integer int1, Integer int2) throws Exception{
+		this.inviter = new Employee(int1 + 1);
+		this.invitee = new Employee(int1);
+		this.activity = new Activity(int2);
+		this.activity.assignEmployee(this.inviter.getId(), 1);
+		this.activity.assignEmployee(this.invitee.getId(), 1);
 		try {
 			this.activity.inviteEmployee(this.inviter, this.invitee);
 		} catch (Exception e) {
 			this.errorMessage.setErrorMessage(e.getMessage());
+			System.out.println(e.getMessage());
 		}
 	}
 
 	@Then("give the exception {string}")
 	public void give_the_exception(String string) {
 		System.out.println(this.errorMessage.getErrorMessage());
-	    assertTrue(this.errorMessage.getErrorMessage().toString() == "Employee being invited is already assigned to activity!");
+	    assertTrue(this.errorMessage.getErrorMessage().toString().equals("Employee being invited is already assigned to activity!"));
 	}
 }
