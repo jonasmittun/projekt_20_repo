3e2ca4a29d1dc8597dd66fbb0a11aaa6992903cd
agentroi <roidalsgard@gmail.com>
2021-05-03 15:31:39 +0200

    Brick of code for Register Menu and controller

--- projekt_20/src/main/java/dtu/company/app/Controller.java
+++ projekt_20/src/main/java/dtu/company/app/Controller.java
@@ -9,7 +9,7 @@ public class Controller {
 	static int CurrentUserID = -1;
 	private static ArrayList<String> userActivities;
 	
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		companyApp = new CompanyApp();
 		view = new View();
 		
@@ -19,9 +19,6 @@ public class Controller {
 
 		//Gather users activities from CompanyApp
 
-		userActivities = companyApp.getUserActivities(CurrentUserID);
-
-		view.RegisterMenu(userActivities);
 
 
 		view.ShutDownText();
@@ -37,6 +34,23 @@ public class Controller {
 		}
 		companyApp.setUser(CurrentUserID);
 		System.out.println("User set in companyApp is now: " + companyApp.getUser());
+	}
+
+	public static void RegisterMenu(int CurrentUserID) throws Exception {
+		//Gets user activities
+		userActivities = companyApp.getUserActivities(CurrentUserID);
+
+		//Runs Register Menu to get users to register hours
+		String s = view.RegisterMenu(userActivities);
+
+		//Saves users decision
+		String projectName = s.substring(0,s.indexOf(':'));
+		int activityID = Integer.parseInt(s.substring(s.indexOf(':'),s.lastIndexOf(':')));
+		int halfHours = Integer.parseInt(s.substring(s.lastIndexOf(':'),s.length()-1));
+
+		//Registers users hours
+		companyApp.registerDaysWork(CurrentUserID,halfHours,activityID,projectName);
+
 	}
 
 }
